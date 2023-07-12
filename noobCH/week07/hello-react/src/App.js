48cc32a933cedf62764a8fb247e9a62cf1acedbb
Dobbymin <79429482+Dobbymin@users.noreply.github.com>
2023-07-12 18:53:08 +0900

    week07

---
+++ noobCH/week07/hello-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    const name = '이재현';
+    return (
+        <div>
+            <h1 classnName="myNme">{name}</h1>
+        </div>
+    );
+}
+
+export default App;
